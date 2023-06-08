2a75873108ac03a9afc149e6ed081a379494e3df
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-06-08 16:50:44 -0700

    Refactoring functions from Spanish app to consts
    Co-authored-by: Zachary Cipolletta <ZackCipolletta@gmail.com>

--- src/DragCard.js
+++ src/DragCard.js
@@ -1,34 +1,98 @@
-// variables needed: selectedId, dropTargetId, matching counter
-
-// drag function: Spanish game has dragStart, dragEnter, dragLeave, dragOver and dragDrop
-// dragStart selects current ID, dragEnter and dragLeave deal with a class ('over') by adding or removing it -- these are only called in the function addEventListeners (bottom of page) -- changes color when hovered
-// dragOver invokes event.preventDefault
-// dragDrop handles removing matches by setting display to none if a match is made, also adds 1 to counter
-
-// checkForMatch function: both ways (dragging English or Spanish ex)
-// JS app works by using switch case and booleans (checkForMatch and checkForMatch2 -- both used in dragDrop function)
-
-// counter function (maybe? Spanish game counts to 5, which was the maximum number of matches provided)
-
-// playAgain function: reset cards, shows End message
-
-// useEffect for addEventListener functions
-
-// create static list of questions and ansers with matching IDs then loop through the list to display on page.
-
 import React, { useState, useEffect, useCallback, useRef } from 'react';
 import './DragCard.css';
-// import Container from 'react-bootstrap';
-// import Row from 'react-bootstrap';
-// import Col from 'react-bootstrap';
-import { Container, Row, Col } from 'react-bootstrap';
+// import { Container, Row, Col } from 'react-bootstrap';
+import { Grid, Paper } from '@mui/material';
 
 function DragCard() {
-  // let selectedId;
-  // let dropTargetId;
-  // let matchingCounter = 0;
+
+  const [selectedId, setSelectedId] = useState(null);
+  const [dropTargetId, setDropTargetId] = useState(null);
+  // may be better to switch this to let matchingCounter = 0;
+  const [matchingCounter, setMatchingCounter] = useState(0);
+
+  // this sets the event listeners when the page is loaded
+  useEffect(() => {
+    addEventListeners();
+
+    return () => {
+      removeEventListeners();
+    };
+  }, []);
 
 
+  // transforming his functions into consts
+  const dragStart = () => {
+    const id = this.id;
+    setSelectedId(id);
+  }
+
+  const dragEnter = () => {
+    this.classList.add('over');
+  }
+
+  const dragLeave = () => {
+    this.classList.remove('over');
+  }
+
+  const dragOver = (ev) => {
+    ev.preventDefault();
+  }
+
+  const dragDrop = () => {
+    const id = this.id;
+    setDropTargetId(id);
+
+    if (checkForMatch(selectedId, dropTargetId)) {
+      document.getElementById(selectedId).style.display = 'none';
+      document.getElementById(dropTargetId).style.display = 'none';
+      console.log('Yay it works!')
+      matchingCounter++;
+    }
+
+    // if (matchingCounter === 5) {
+    //   endMessage.style.display = 'block';
+    // }
+
+    this.classList.remove('over');
+  }
+
+  // check id variable names
+  const checkForMatch = (selectedId, dropTargetId) => {
+    if (selectedId === dropTargetId) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+
+  const draggableListItems = document.querySelectorAll('.draggableItem');
+
+  // something needs fixed here
+  const addEventListeners = () => {
+    draggableListItems.forEach(item => {
+      item.addEventListener('dragstart', dragStart);
+      item.addEventListener('dragenter', dragEnter);
+      item.addEventListener('drop', dragDrop);
+      item.addEventListener('dragover', dragOver);
+      item.addEventListener('dragleave', dragLeave);
+    })
+  }
+
+  function removeEventListeners() {
+    draggableListItems.forEach((item) => {
+      item.removeEventListener('dragstart', dragStart);
+      item.removeEventListener('dragenter', dragEnter);
+      item.removeEventListener('drop', dragDrop);
+      item.removeEventListener('dragover', dragOver);
+      item.removeEventListener('dragleave', dragLeave);
+    });
+  }
+
+
+
+  // hard coded questions
   const questionList = [
     {
       q: 'What color is the sky?',
@@ -77,35 +141,37 @@ function DragCard() {
 
   ];
 
-  // useEffect(() => {
-
-  // })
-
-
+  // checking to make sure ID match questions and answers
   console.log('Logging question IDs:', questionList.map((question) => `${question.q} ${question.id}`));
-  
+
   return (
-    <>
-      <div>
-        <h3>Questions:</h3>
-        {questionList.map((question, id) => (
-          <p className='questionList' key={id} draggable="true">
-            {question.q}
-          </p>
-        ))}
-      </div>
-  
-      <div >
-        <h3>Answers:</h3>
-        {answerList.map((answer, id) => (
-          <p className='answerList' key={id} draggable="true">
-            {answer.a}
-          </p>
-        ))}
-      </div>
-    </>
+    <Grid container spacing={2}>
+      <Grid item xs={6}>
+        <Paper>
+          <h3>Questions:</h3>
+          {questionList.map((question, id) => (
+            <p className='questionList draggableItem'
+              key={id}
+              draggable='true'>
+              {question.q}</p>
+          ))}
+        </Paper>
+      </Grid>
+      <Grid item xs={6}>
+        <Paper>
+          <h3>Answers:</h3>
+          {answerList.map((answer, id) => (
+            <p className='answerList draggableItem'
+              key={id}
+              draggable='true'>
+              {answer.a}</p>
+          ))}
+        </Paper>
+      </Grid>
+    </Grid>
   );
 }
 
 
-export default DragCard;
+
+export default DragCard
