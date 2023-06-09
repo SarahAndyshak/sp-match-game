9a89c88a3f5e22ddb6f72970f839f9caedd916f5
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-06-09 16:45:59 -0700

    Add information to access firebase questions, all working
    Co-authored-by: Zachary Cipolletta <ZackCipolletta@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import DragCard from './DragCard';
 import DragCardParent from './DragCardParent';
+import { collection, addDoc, doc, updateDoc, onSnapshot, deleteDoc } from "firebase/firestore";
+import { db } from './firebase';
 
 function App() {
+  const [studyset, setStudyset] = useState([]);
+  console.log('studyset', studyset)
+  useEffect(() => {
+    getStudySet();
+  }, [])
+
+  const [questions, setQuestions] = useState([]);
+  const [answers, setAnswers] = useState([]);
+  console.log('answers', answers)
+  console.log('questions', questions)
+
+
+  const getStudySet = async () => {
+    try {
+      const studysetRef = doc(db, 'sets', '2hIITNZFinxhpiCttxgL');
+
+      onSnapshot(studysetRef, (doc) => {
+        setStudyset(doc.data().cards)
+      })
+
+    } catch (error) {
+      console.log('Error getting single study set', error);
+    }
+  }
+
+  useEffect(() => {
+    if (studyset) {
+      // initiate question array
+      let qs = [];
+      // initiate answer array
+      let as = [];
+      studyset.map((x) => {
+        qs.push({q: x.question, id:x.id});
+        as.push({a: x.correctAnswerList[0].answer, id:x.id})
+      })
+      setQuestions(qs);
+      setAnswers(as);
+    }
+  }, [studyset])
+
   return (
     <div className="App">
-      <DragCard />
+      <DragCard answers={answers} questions={questions} />
       {/* <DragCardParent /> */}
     </div>
   );
--- src/DragCard.js
+++ src/DragCard.js
@@ -1,241 +1,8 @@
-// import React, { useState, useEffect } from 'react';
-// import './DragCard.css';
-// import { Grid, Paper } from '@mui/material';
-
-// const DragCard = (props) => {
-//   // const { initAnswerList, initQuestionList, setInitAnswerList, setInitQuestionList} = props;
-//   // const [selectedId, setSelectedId] = useState(null);
-//   const [dropTargetId, setDropTargetId] = useState(null);
-//   const [matchingCounter, setMatchingCounter] = useState(0);
-//   let startId = null;
-//   let questionList = [
-//     {
-//       q: 'What color is the sky?',
-//       id: 1,
-//     },
-//     {
-//       q: 'What is your name?',
-//       id: 2,
-//     },
-//     {
-//       q: 'Nice to meet you',
-//       id: 3,
-//     },
-//     {
-//       q: 'What is the capital of Oregon',
-//       id: 4,
-//     },
-//     {
-//       q: 'What is the national bird',
-//       id: 5,
-//     },
-//   ];
-
-//   let answerList = [
-//     {
-//       a: 'Blue',
-//       id: 1,
-//     },
-//     {
-//       a: '¿Cómo te llamas?',
-//       id: 2,
-//     },
-//     {
-//       a: 'Mucho gusto',
-//       id: 3,
-//     },
-//     {
-//       a: 'Salem',
-//       id: 4,
-//     },
-//     {
-//       a: 'the bald eagle',
-//       id: 5,
-//     },
-//   ];
-//   console.log('answerList', answerList)
-
-//   const [completedAnswers, setCompletedAnswers] = useState([]);
-//   const [completedQuestions, setCompletedQuestions] = useState([]);
-
-  
-//   useEffect(() => {
-//     console.log('rerendered')
-//   }, [])
-
-
-//   // const draggableListItems = document.querySelectorAll('.draggableItem');
-  
-//   // const addEventListeners = () => {
-//   //   // const draggableListItems = document.querySelectorAll('.draggableItem');
-
-//   //   draggableListItems.forEach((item) => {
-//   //     item.addEventListener('dragstart', dragStart);
-//   //     item.addEventListener('dragenter', dragEnter);
-//   //     item.addEventListener('drop', dragDrop);
-//   //     item.addEventListener('dragover', dragOver);
-//   //     item.addEventListener('dragleave', dragLeave);
-//   //   });
-//   // };
-
-//   // const removeEventListeners = () => {
-//   //   // const draggableListItems = document.querySelectorAll('.draggableItem');
-
-//   //   draggableListItems.forEach((item) => {
-//   //     item.removeEventListener('dragstart', dragStart);
-//   //     item.removeEventListener('dragenter', dragEnter);
-//   //     item.removeEventListener('drop', dragDrop);
-//   //     item.removeEventListener('dragover', dragOver);
-//   //     item.removeEventListener('dragleave', dragLeave);
-//   //   });
-//   // };
-
-//   const dragStart = (event) => {
-//     startId = event.target.id;
-//     // setSelectedId(startId);
-//     console.log('drag started', event)
-//   };
-
-//   const dragEnter = (event) => {
-//     event.target.classList.add('over');
-//   };
-
-//   const dragLeave = (event) => {
-//     event.target.classList.remove('over');
-//   };
-
-//   const dragOver = (event) => {
-//     event.preventDefault();
-//   };
-  
-//   // const checkForMatch = (selectedId, dropTargetId) => {
-//   //   return selectedId === dropTargetId;
-//   // };
-
-//   const dragDrop = (event) => {
-//     // console.log('selectedId', selectedId);
-//     event.preventDefault();
-//     // const startId = event.srcElement.id; // currently thinks this value is null
-//     // console.log('startId', startId);
-//     const dropId = event.target.id;
-
-//     setDropTargetId(dropId);
-//     // const endId = parseInt(dropId);
-
-// // need to parseInt of selectedId and dropId to compare them
-
-//     console.log(startId === event.target.id)
-
-//     if (startId === event.target.id) {
-//       // document.getElementById(selectedId).style.display = 'none';
-//       // document.getElementById(id).style.display = 'none';
-//       console.log('Yay it works!');
-//       onCompletion();
-//       // setMatchingCounter((prevCounter) => prevCounter + 1);
-//       // console.log(`${matchingCounter}`);
-//     } else {
-//       console.log('try again')
-//     }
-
-//     event.target.classList.remove('over');
-//   };
-
-//   // console log to check selectedId and dropTargetId
-//   useEffect(() => {
-//     // console.log(selectedId)
-//     console.log(dropTargetId)
-//   }, [dropTargetId, dropTargetId]);
-
-
-//   useEffect(() => {
-//     // addEventListeners();
-//     const draggableListItems = document.querySelectorAll('.draggableItem');
-
-//     draggableListItems.forEach((item) => {
-//       item.addEventListener('dragstart', dragStart);
-//       item.addEventListener('dragenter', dragEnter);
-//       item.addEventListener('drop', dragDrop);
-//       item.addEventListener('dragover', dragOver);
-//       item.addEventListener('dragleave', dragLeave);
-//     });
-
-//     return () => {
-//       // removeEventListeners();
-//         const draggableListItems = document.querySelectorAll('.draggableItem');
-
-//       draggableListItems.forEach((item) => {
-//         item.removeEventListener('dragstart', dragStart);
-//         item.removeEventListener('dragenter', dragEnter);
-//         item.removeEventListener('drop', dragDrop);
-//         item.removeEventListener('dragover', dragOver);
-//         item.removeEventListener('dragleave', dragLeave);
-//       });
-//     };
-//   }, []);
-
-//   // console.log('answerList', answerList)
-//   // console.log('questionList', questionList)
-//   // console.log('completedAnswers', completedAnswers)
-//   // console.log('completedQuestions', completedQuestions)
-
-//   const onCompletion = () => {
-//     const copyAnswerList = [...answerList];
-//     const copyQuestionList = [...questionList];
-
-//     const oldAnswerArray = answerList.filter(x => x.id != startId);
-//     console.log('oldAnswerArray', oldAnswerArray);
-//     const newAnswerArray = answerList.filter(x => x.id == startId);
-//     console.log('newAnswerArray', newAnswerArray);
-
-//     const oldQuestionArray = questionList.filter(x => x.id != startId);
-//     // console.log('oldQuestionArray', oldQuestionArray);
-//     const newQuestionArray = questionList.filter(x => x.id == startId);
-//     // console.log('newQuestionArray', newQuestionArray);
-
-//     questionList = oldQuestionArray;
-//     answerList = oldAnswerArray;
-//     setCompletedAnswers((completedAnswers) => ([...completedAnswers, newAnswerArray[0]]));
-//     setCompletedQuestions((completedQuestions) => ([...completedQuestions, newQuestionArray[0]]));
-//   }
-
-//   console.log('questionList', questionList);
-//   console.log('answerList', answerList);
-
-//   return (
-//     <Grid container spacing={2}>
-//       <Grid item xs={6}>
-//         <Paper>
-//           <h3>Questions:</h3>
-//           {questionList?.map((question, id) => (
-//             <p className="questionList draggableItem" key={id} draggable="true" id={question.id}>
-//               {question.q}
-//             </p>
-//           ))}
-//         </Paper>
-//       </Grid>
-//       <Grid item xs={6}>
-//         <Paper>
-//           <h3>Answers:</h3>
-//           {answerList?.map((answer, id) => (
-//             <p className="answerList draggableItem" key={id} draggable="true" id={answer.id}>
-//               {answer.a}
-//             </p>
-//           ))}
-//         </Paper>
-//       </Grid>
-//     </Grid>
-//   );
-// }
-
-// export default DragCard;
-
-
-
 import React, { useState, useEffect } from 'react';
 import './DragCard.css';
 import { Grid, Paper } from '@mui/material';
 
-function DragCard() {
+function DragCard({answers, questions}) {
   const [selectedId, setSelectedId] = useState(null);
   const [dropTargetId, setDropTargetId] = useState(null);
   const [matchingCounter, setMatchingCounter] = useState(0);
@@ -279,43 +46,47 @@ function DragCard() {
 
   //by using a useEffect we solve our issue of state not being updated when the if then statement is called to check for equality of selectedId and dropTargetId
   useEffect(() => {
-    // if the id of the selected element equals the id of the target element, we continue with our function.
-    if (selectedId === dropTargetId) {
-      console.log('Yay it works!');
-      const selectedElement = document.getElementById(selectedId);
-      const dropTargetElement = document.getElementById(dropTargetId);
-    
-
-      // this is a null check, so if these variables are not assigned values, the if then statement will not run.
-      if (selectedElement && dropTargetElement) {
-        // selectedElement.style.display = 'none';
-        selectedElement.style.display = 'none';
-
-        // Find matching answer element with the same ID value and set its display to 'none'
-        const matchingAnswerElement = document.querySelector(
-          `.answerList.draggableItem[id="${selectedId}"]`
-        );
-
-        // same here - if matchingAnswerElement is not assigned a value, the statement will not run. This prevents the error: Cannot read properties of null (reading 'style')
-        if (matchingAnswerElement) {
-          matchingAnswerElement.style.display = 'none';
+    if (selectedId && dropTargetId) {
+      // if the id of the selected element equals the id of the target element, we continue with our function.
+      if (selectedId === dropTargetId) {
+        console.log('Yay it works!');
+        const selectedElement = document.getElementById(selectedId);
+        const dropTargetElement = document.getElementById(dropTargetId);
+      
+  
+        // this is a null check, so if these variables are not assigned values, the if then statement will not run.
+        if (selectedElement && dropTargetElement) {
+          // selectedElement.style.display = 'none';
+          selectedElement.style.display = 'none';
+  
+          // Find matching answer element with the same ID value and set its display to 'none'
+          const matchingAnswerElement = document.querySelector(
+            `.answerList.draggableItem[id="${selectedId}"]`
+          );
+  
+          // same here - if matchingAnswerElement is not assigned a value, the statement will not run. This prevents the error: Cannot read properties of null (reading 'style')
+          if (matchingAnswerElement) {
+            matchingAnswerElement.style.display = 'none';
+          }
         }
+      } else {
+        console.log('try again');
       }
-    } else {
-      console.log('try again');
+
+      const resetDragStyles = () => {
+        const draggableItems = document.querySelectorAll('.draggableItem');
+        draggableItems.forEach((item) => {
+          item.classList.remove('over');
+        });
+      };
+
+      resetDragStyles();
     }
-
-    const resetDragStyles = () => {
-      const draggableItems = document.querySelectorAll('.draggableItem');
-      draggableItems.forEach((item) => {
-        item.classList.remove('over');
-      });
-    };
-
-    resetDragStyles();
+    
   }, [selectedId, dropTargetId]);
 
   useEffect(() => {
+    if (questions && answers) {
     const draggableListItems = document.querySelectorAll('.draggableItem');
     draggableListItems.forEach((item) => {
       item.addEventListener('dragstart', dragStart);
@@ -334,29 +105,31 @@ function DragCard() {
         item.removeEventListener('dragover', dragOver);
         item.removeEventListener('dragleave', dragLeave);
       });
-    };
-  }, []);
+      };
+    }
+  }, [questions, answers]);
 
 
   useEffect(() => {
-    // Check if all elements are hidden
-    const allHidden = questionList.every((question) => {
-      const questionElement = document.getElementById(question.id);
-      return questionElement.style.display === 'none';
-    }) && answerList.every((answer) => {
-      const answerElement = document.getElementById(answer.id);
-      return answerElement.style.display === 'none';
-    });
-  
-    if (allHidden) {
-      setAllHidden(true);
-      console.log('All elements are hidden');
+    if (selectedId && dropTargetId && questions) {
+      // Check if all elements are hidden
+      const allHidden = questions.every((question) => { // << change questions to questionList
+        const questionElement = document.getElementById(question.id);
+        return questionElement.style.display === 'none';
+      }) && answers.every((answer) => { // << change answers to answerLIst
+        const answerElement = document.getElementById(answer.id);
+        return answerElement.style.display === 'none';
+      });
+    
+      if (allHidden) {
+        setAllHidden(true);
+        console.log('All elements are hidden');
+      }
     }
-  }, [selectedId, dropTargetId]);
-  
+  }, [selectedId, dropTargetId, questions]);
 
 
-  const questionList = [
+    const questionList = [
     {
       q: 'What color is the sky?',
       id: 1,
@@ -402,13 +175,14 @@ function DragCard() {
     },
   ];
 
+
   return (
       <Grid container spacing={2}>
         <Grid item xs={6}>
           <Paper>
-            <h3>Questions 777:</h3>
-            {questionList.map((question, id) => (
-              <p className="questionList draggableItem"
+            <h3>Questions:</h3>
+            {questions?.map((question, id) => ( // << change questions to questionList
+              <p className="draggableItem questionList"
                 key={id}
                 draggable="true"
                 id={question.id}>
@@ -420,8 +194,8 @@ function DragCard() {
         <Grid item xs={6}>
           <Paper>
             <h3>Answers:</h3>
-            {answerList.map((answer, id) => (
-              <p className="answerList draggableItem"
+            {answers?.map((answer, id) => ( // << change answers to answerList
+              <p className="draggableItem answerList"
                 key={id}
                 draggable="true"
                 id={answer.id}>
@@ -441,3 +215,7 @@ function DragCard() {
 }
 
 export default DragCard;
+
+
+
+
